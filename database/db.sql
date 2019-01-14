6e7105576534e65831a730549013613bcafeca43
Gerardo Lozano <gerardolozano51@icloud.com>
2019-01-13 18:57:39 -0600

    BD structure
    UPDATE

--- database/db.sql
+++ database/db.sql
@@ -4,33 +4,180 @@ CREATE DATABASE APP;
 -- to use database
 use APP;
 
--- creating a new table
-CREATE TABLE estado (
-  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  nombre VARCHAR(50) NOT NULL,
-  habilitado int(1) NOT NULL DEFAULT 1
-);
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 14-01-2019 a las 00:55:40
+-- Versión del servidor: 5.6.38
+-- Versión de PHP: 7.2.1
 
-CREATE TABLE municipio (
-  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  nombre VARCHAR(50) NOT NULL,
-  id_estado int(6) NOT NULL,
-  habilitado int(1) NOT NULL DEFAULT 1
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
+--
+-- Base de datos: `APP`
+--
 
-CREATE TABLE usuario (
-  id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  usuario VARCHAR(50) NOT NULL,
-  password VARCHAR(200) NOT NULL,
-  token VARCHAR(200) NOT NULL,
-  rol INT(2) NOT NULL DEFAULT 0
-);
+-- --------------------------------------------------------
 
--- to show all tables
-show tables;
+--
+-- Estructura de tabla para la tabla `calificacion`
+--
 
--- to describe table
-describe customer;
+CREATE TABLE `calificacion` (
+  `id` int(11) NOT NULL,
+  `id_servicio` int(11) NOT NULL,
+  `calificacion` int(11) NOT NULL,
+  `comentario` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `direcciones`
+--
+
+CREATE TABLE `direcciones` (
+  `id` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `direccion` varchar(200) NOT NULL,
+  `direccion_maps` varchar(200) NOT NULL,
+  `numero_maps` varchar(20) NOT NULL,
+  `estado` varchar(50) NOT NULL,
+  `codigo_postal_maps` varchar(10) NOT NULL,
+  `pais` varchar(30) NOT NULL,
+  `telefono` varchar(15) NOT NULL,
+  `referencias` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estado`
+--
+
+CREATE TABLE `estado` (
+  `id` int(6) UNSIGNED NOT NULL,
+  `id_google_maps` varchar(50) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `habilitado` int(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `estado`
+--
+
+INSERT INTO `estado` (`id`, `id_google_maps`, `nombre`, `habilitado`) VALUES
+(1, 'N.L.', 'Nuevo León', 1),
+(2, 'Pue.', 'Puebla', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `municipio`
+--
+
+CREATE TABLE `municipio` (
+  `id` int(6) UNSIGNED NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `id_estado` int(6) NOT NULL,
+  `habilitado` int(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `municipio`
+--
+
+INSERT INTO `municipio` (`id`, `nombre`, `id_estado`, `habilitado`) VALUES
+(1, 'Guadalupe', 1, 1),
+(2, 'Monterrey', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `token` varchar(200) NOT NULL,
+  `rol` int(2) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `calificacion`
+--
+ALTER TABLE `calificacion`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `direcciones`
+--
+ALTER TABLE `direcciones`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `estado`
+--
+ALTER TABLE `estado`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `municipio`
+--
+ALTER TABLE `municipio`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `calificacion`
+--
+ALTER TABLE `calificacion`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `direcciones`
+--
+ALTER TABLE `direcciones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `estado`
+--
+ALTER TABLE `estado`
+  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `municipio`
+--
+ALTER TABLE `municipio`
+  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
 
 
